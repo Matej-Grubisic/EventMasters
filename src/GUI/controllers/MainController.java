47355cf8480b3c41220048bdd52e5ed13e3b6837
Matej Grubišić <grubisic401@gmail.com>
2024-03-15 10:53:15 +0100

    First commit

---
+++ src/GUI/controllers/MainController.java
@@ -0,0 +1,17 @@
+package GUI.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+import java.util.Random;
+
+public class MainController {
+    public Button btnBig;
+    Random rand = new Random();
+
+    public void click(ActionEvent actionEvent) {
+        System.out.println("Weeeeeeeee!");
+        btnBig.setLayoutX(rand.nextDouble(700));
+        btnBig.setLayoutY(rand.nextDouble(450));
+    }
+}
