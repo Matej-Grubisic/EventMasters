e4a67577011fc6d889e316fe9834eb0f369891e4
vedrancop <146172096+vedrancop@users.noreply.github.com>
2024-03-15 13:56:39 +0100

    log in whtout css

--- src/GUI/controllers/MainController.java
+++ src/GUI/controllers/MainController.java
@@ -7,11 +7,15 @@ import java.util.Random;
 
 public class MainController {
     public Button btnBig;
+    public Button logInBTN;
     Random rand = new Random();
 
     public void click(ActionEvent actionEvent) {
         System.out.println("Weeeeeeeee!");
         btnBig.setLayoutX(rand.nextDouble(700));
         btnBig.setLayoutY(rand.nextDouble(450));
+    }
+
+    public void ClickLogInBTN(ActionEvent actionEvent) {
     }
 }
