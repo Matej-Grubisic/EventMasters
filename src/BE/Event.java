2b1f8b281324781c61429a4e2f6bc18fe444e209
alekss <anoviconoks@gmail.com>
2024-03-20 15:55:20 +0100

    Getters and setter for the Event.

--- src/BE/Event.java
+++ src/BE/Event.java
@@ -1,4 +1,31 @@
 package BE;
 
 public class Event {
+    private String name;
+    private String location;
+    private double time;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public double getTime() {
+        return time;
+    }
 }
