725ae59b1e192e3173dc887e8611449c66716f16
alekss <anoviconoks@gmail.com>
2024-04-17 11:43:35 +0200

    Junit test

---
+++ Test/GUI/controllers/LogInControllerTest.java
@@ -0,0 +1,17 @@
+package GUI.controllers;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogInControllerTest {
+
+
+
+    @Test
+    void clickLogInBTN() {
+
+    }
+
+}
